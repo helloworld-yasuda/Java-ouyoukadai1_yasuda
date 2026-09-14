e1c9aa26c627e5808ba99aad2b1ff6ceb235ccb9
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-09-14 10:46:29 +0900

    不要なimportを削除

--- src/main/java/com/example/dockerapi/controller/HelloController.java
+++ src/main/java/com/example/dockerapi/controller/HelloController.java
@@ -1,7 +1,5 @@
 package com.example.dockerapi.controller;
 
-import java.awt.desktop.UserSessionEvent;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
