9dc84e97866b14abfc2e960cf8acad79fe9939bc
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-09-15 12:01:40 +0900

    以前のコメントアウトの修正

--- src/main/java/com/example/dockerapi/controller/HelloController.java
+++ src/main/java/com/example/dockerapi/controller/HelloController.java
@@ -40,10 +40,10 @@ public class HelloController {
     @GetMapping("/users/{user_id}")
     public Users getUsersDetail(@PathVariable("user_id") Long id){
         String sql = "SELECT id,name,email FROM users WHERE id = ?";//DBに命令するSQL文を変数sqlに格納する
-        Users users = jdbcTemplate.queryForObject(//jdbcTemplate.queryForObject()はDBに対してSQLを複数行取得する
-            sql,//第一引数：命令文を渡す　BeanPropertyRowMapperはDBから返ってきたデータを列名とクラスのプロパティを結びつける
-            BeanPropertyRowMapper.newInstance(Users.class),//第二引数：列名とフィールド名を対応付けてjavaのデータに変換
-            id//第三引数以降：命令文の？(プレースホルダー)部分に順に代入　newInstanceメソッドはクラスを指定することでこのクラスの形に自動変換してくれるインスタンスを作る
+        Users users = jdbcTemplate.queryForObject(//jdbcTemplate.queryForObject()はDBから一件取得する
+            sql,//命令文を渡す　BeanPropertyRowMapperはDBから返ってきたデータを列名とクラスのプロパティを結びつける
+            BeanPropertyRowMapper.newInstance(Users.class),//列名とフィールド名を対応付けてjavaのデータに変換
+            id//webURLからidを代入　
         );
         return users;//得られたデータリストをwebブラウザやcurlに返す
     }    
