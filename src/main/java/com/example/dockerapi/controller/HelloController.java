3374e79a317a0666990abf8aebeed85754701415
安田秀士 <suzukidaisuke@suzukitaiyokunoMacBook-Pro.local>
2026-09-17 10:08:58 +0900

    コメントアウトのインデント修正、不要なコメントアウトを削除

--- src/main/java/com/example/dockerapi/controller/HelloController.java
+++ src/main/java/com/example/dockerapi/controller/HelloController.java
@@ -14,44 +14,46 @@ import users.Users;
 @RequestMapping("/api")
 public class HelloController {
 
-    @Autowired
-    private JdbcTemplate jdbcTemplate;
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
-    @GetMapping("/hello")
-    public String sayHello() {
-        return "Hello, Docker World!";
-    }
+	@GetMapping("/hello")
+	public String sayHello() {
+		return "Hello, Docker World!";
+	}
 
-    @GetMapping("/hoge")
-    public String sayHoge() {
-        return "hogehogehoge";
-    }
-    
-//    課題1 エンドポイントにGET通信をしたときに、レスポンスが返却されるようにする
-    @GetMapping("/users")
-    public Users getUsers() { 
-    	Users users = new Users(1,"John Doe","john.doe@example.com");
-    	return users;
-    }
-    
-    //課題2 エンドポイントにGET通信をしたときに、レスポンスが返却されるようにする エンドポイント：http://localhost:8080/users/{user_id}
-    @GetMapping("/users/{user_id}")
-    public Users getUsersDetail(@PathVariable("user_id") Long id){
-        String sql = "SELECT id,name,email FROM users WHERE id = ?";//DBに命令するSQL文を変数sqlに格納する
-        Users users = jdbcTemplate.queryForObject(//jdbcTemplate.queryForObject()はDBから一件取得する
-            sql,//命令文を渡す　BeanPropertyRowMapperはDBから返ってきたデータを列名とクラスのプロパティを結びつける
-            BeanPropertyRowMapper.newInstance(Users.class),//列名とフィールド名を対応付けてjavaのデータに変換
-            id//webURLからidを代入　
-        );
-        return users;//得られたデータリストをwebブラウザやcurlに返す
-    }    
-    @GetMapping("/check-db")
-    public String checkDbConnection() {
-        try {
-            jdbcTemplate.queryForObject("SELECT 1", Integer.class); // MySQLへの接続確認
-            return "Database connection is successful!";
-        } catch (Exception e) {
-            return "Database connection failed!";
-        }
-    }
+	@GetMapping("/hoge")
+	public String sayHoge() {
+		return "hogehogehoge";
+	}
+
+	// 課題1 エンドポイントにGET通信をしたときに、レスポンスが返却されるようにする
+	@GetMapping("/users")
+	public Users getUsers() {
+		Users users = new Users(1, "John Doe", "john.doe@example.com");
+		return users;
+	}
+
+	// 課題2 エンドポイントにGET通信をしたときに、レスポンスが返却されるようにする
+	// エンドポイント：http://localhost:8080/users/{user_id}
+	@GetMapping("/users/{user_id}")
+	public Users getUsersDetail(@PathVariable("user_id") Long id) {
+		String sql = "SELECT id,name,email FROM users WHERE id = ?";
+		Users users = jdbcTemplate.queryForObject(
+				sql, 
+				BeanPropertyRowMapper.newInstance(Users.class), 
+				id
+		);
+		return users;// 得られたデータリストをwebブラウザやcurlに返す
+	}
+
+	@GetMapping("/check-db")
+	public String checkDbConnection() {
+		try {
+			jdbcTemplate.queryForObject("SELECT 1", Integer.class); // MySQLへの接続確認
+			return "Database connection is successful!";
+		} catch (Exception e) {
+			return "Database connection failed!";
+		}
+	}
 }
